5a23ba6a4d88b6e6b2daf0042038414bd1d9e1c2
iubaldo <ianzachary.u@gmail.com>
2022-09-25 14:23:39 -0500

    lab 3: got bottom row to print correctly

--- CS_362_lab_3/iubaldo2lab3/iubaldo2lab3.ino
+++ CS_362_lab_3/iubaldo2lab3/iubaldo2lab3.ino
@@ -9,7 +9,6 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
   Serial.begin(9600);
-  // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
 }
@@ -24,17 +23,17 @@ void loop() {
     toPrint = top.substring(strIdx) + top.substring(0, remaining);
   }
   
-  sprintf(row1, "%s", toPrint);
-  sprintf(row2, "       %s      ", "Ian");
+  // sprintf(row1, "%s", toPrint);
+  sprintf(row2, "      %s    ", "Ian");
   
   lcd.setCursor(0, 0);
   lcd.print(toPrint);
+  lcd.setCursor(0, 1);
   lcd.print(row2);
-
-  Serial.println(toPrint);
   
-  strIdx++;
-  if (strIdx >= top.length())
+  // Serial.println(toPrint);
+  
+  if (++strIdx >= top.length())
     strIdx = 0;
   
   delay(400);
